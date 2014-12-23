ef26ceda6ed96a33417e1ba653dd965cdfbae2d8
Mahmood Ramzani <ramzani.mahmood@gmail.com>
2014-12-23 21:45:47 +0330

    * echo sampled for tcp

---
+++ Network/TCP/Echo/Client/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string host = "127.0.0.1";
+            int port = 9001;
+            IPAddress address = IPAddress.Parse(host);
+
+            Socket socket=new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+
+            socket.Connect(address,port);
+
+            string data = "Mahmood";
+            byte[] dataB = Encoding.UTF8.GetBytes(data);
+
+            socket.Send(dataB);
+
+            byte[] buffer=new byte[1024];
+            socket.Receive(buffer);
+
+            socket.Close();
+
+            string dataEcho = Encoding.UTF8.GetString(buffer);
+
+            Console.WriteLine(dataEcho);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Network/TCP/Echo/Server/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string host = "";
+            int port = 9001;
+
+            IPAddress address = IPAddress.Any;
+            EndPoint endPoint=new IPEndPoint(address,port);
+
+
+            Socket socket=new Socket(AddressFamily.InterNetwork,SocketType.Stream, ProtocolType.Tcp);
+
+            socket.Bind(endPoint);
+            socket.Listen(1);
+
+            while (true)
+            {
+                Socket newSocket = socket.Accept();
+
+                IPEndPoint remoteEndPoint = (IPEndPoint) newSocket.RemoteEndPoint;
+                string newClientMsg = string.Format("new client connected form {0}:{1}", remoteEndPoint.Address.ToString(),
+                    remoteEndPoint.Port);
+
+                Console.WriteLine(newClientMsg);
+
+                byte[] buffer=new byte[1024];
+                newSocket.Receive(buffer);
+
+                string data = Encoding.UTF8.GetString(buffer);
+                Console.WriteLine(data);
+
+                newSocket.Send(buffer);
+
+                newSocket.Close();
+            }
+        }
+    }
+}
