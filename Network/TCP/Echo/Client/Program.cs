89fccb76dcfd2f183ab295fd12632cf24f94ccd8
Mahmood Ramzani <ramzani.mahmood@gmail.com>
2014-12-23 19:56:07 +0330

    * echo sample in tcp

---
+++ Network/TCP/Echo/Client/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress address = IPAddress.Parse("127.0.0.1");
+            int port = 9001;
+
+            string msg = "Mahmood";
+            byte[] msgB = Encoding.UTF8.GetBytes(msg);
+
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(address,port);
+
+            socket.Send(msgB);
+
+            byte[] buffer=new byte[1024];
+
+            socket.Receive(buffer);
+
+            string data = Encoding.UTF8.GetString(buffer);
+
+            Console.WriteLine(data);
+
+            socket.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Network/TCP/Echo/Server/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress address=IPAddress.Parse("127.0.0.1");
+            int port = 9001;
+
+            EndPoint endPoint=new IPEndPoint(address,port);
+            Socket socket=new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+
+            socket.Bind(endPoint);
+            socket.Listen(1);
+
+            while (true)
+            {
+                Socket newSocket = socket.Accept();
+
+                byte[] buffer=new byte[1024];
+                newSocket.Receive(buffer);
+
+                string data = Encoding.UTF8.GetString(buffer);
+
+                Console.WriteLine(data);
+
+                newSocket.Send(buffer);
+
+                newSocket.Close();
+            }
+        }
+    }
+}
