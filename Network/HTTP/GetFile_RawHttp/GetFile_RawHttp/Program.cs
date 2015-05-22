ff5265e8b178dd4fa725118cfc24e0b9742e764a
Mahmood Ramzani <ramzani.mahmood@gmail.com>
2015-05-22 09:56:09 +0430

    samples for raw http

---
+++ Network/HTTP/GetFile_RawHttp/GetFile_RawHttp/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetFile_RawHttp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileName = "Keygen.jar";
+            var getCmd = string.Format("GET /{0} HTTP/1.1", fileName);
+
+            var host = "192.168.1.3";
+            var hostCmd = string.Format("Host: {0}", host);
+            string delimter = "\r\n";
+
+            var requestArray = new string[]
+            {
+                getCmd,
+                hostCmd,
+                "Connection: Keep-Alive",
+                "",
+                "",
+            };
+
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(host, 80);
+
+            var request = string.Join(delimter, requestArray);
+            var requestB = Encoding.Default.GetBytes(request);
+
+            socket.Send(requestB);
+
+            int totalBytesRead = 0;
+            int readBytes = 0;
+            byte[] buffer = new byte[1024];
+
+            MemoryStream memoryStream = new MemoryStream();
+
+            readBytes = socket.Receive(buffer);
+
+            while (readBytes > 0)
+            {
+                memoryStream.Write(buffer, 0, readBytes);
+                totalBytesRead += readBytes;
+
+                readBytes = socket.Receive(buffer);
+            }
+
+            var dataB = memoryStream.ToArray();
+            var data = Encoding.Default.GetString(dataB);
+
+            var splitedData = data.Split(new string[] { delimter+delimter}, StringSplitOptions.None);
+            var header = splitedData[0];
+            var body = splitedData[1];
+
+            Console.WriteLine(header);
+
+            var newFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), fileName);
+
+            var writer = new FileStream(newFilePath, FileMode.Create);
+            writer.Flush();
+            writer.Close();
+
+            writer = new FileStream(newFilePath, FileMode.Append);
+
+            var bodyB = Encoding.Default.GetBytes(body);
+
+            writer.Write(bodyB,0,bodyB.Length);
+            writer.Flush();
+            writer.Close();
+
+            socket.Close();
+
+            Console.WriteLine(totalBytesRead);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Network/HTTP/GetTextFile_RawHttp/GetTextFile_RawHttp/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetTextFile_RawHttp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var requestArray = new string[]
+            {
+                "GET /hello.txt HTTP/1.1",
+                "Host: 192.168.1.3",
+                "Connection: Close",
+                "",
+                "",
+            };
+
+            var socket=new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+            socket.Connect("192.168.1.3",80);
+
+            var request = string.Join("\r\n", requestArray);
+            var requestB = Encoding.ASCII.GetBytes(request);
+
+            socket.Send(requestB);
+
+            int totalBytesRead = 0;
+            int readBytes = 0;
+            byte[] buffer=new byte[1024];
+
+            MemoryStream memoryStream=new MemoryStream();
+
+            readBytes = socket.Receive(buffer);
+
+            while (readBytes>0)
+            {
+                memoryStream.Write(buffer,0,readBytes);
+                totalBytesRead += readBytes;
+
+                readBytes = socket.Receive(buffer);
+            }
+
+            var dataB = memoryStream.ToArray();
+
+            var data = Encoding.ASCII.GetString(dataB);
+
+            //Console.WriteLine(data);
+
+            var dataSplit = data.Split(new string[]{"\r\n\r\n"},StringSplitOptions.None);
+            var header = dataSplit[0];
+            var body = dataSplit[1];
+
+            Console.WriteLine(header);
+            Console.WriteLine(body);
+
+            socket.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
