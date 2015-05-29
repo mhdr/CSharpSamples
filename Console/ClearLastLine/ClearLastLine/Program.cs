b91771c2d3b43cd67cfe5aea69ed3693ab25d2b9
Mahmood Ramzani <ramzani.mahmood@gmail.com>
2015-05-29 08:42:31 +0430

    console samples

---
+++ Console/ClearLastLine/ClearLastLine/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClearLastLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("-");
+            Thread.Sleep(1000 * 3);
+            Console.Write("\r");
+
+            Console.Write("=");
+            Thread.Sleep(1000 * 3);
+            Console.Write("\r");
+
+            Console.Write("=-");
+            Thread.Sleep(1000 * 3);
+            Console.Write("\r");
+
+            Console.Write("==");
+            Thread.Sleep(1000 * 3);
+            Console.Write("\r");
+
+            Console.Write("==-");
+            Thread.Sleep(1000 * 3);
+            Console.Write("\r");
+
+            Console.Write("===");
+            Thread.Sleep(1000 * 3);
+            Console.Write("\r");
+
+            Console.Write("===-");
+            Thread.Sleep(1000 * 3);
+            Console.Write("\r");
+
+            Console.Write("====");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Console/SetPosition/SetPosition/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetPosition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // current column
+            Console.Write(Console.CursorLeft);
+
+            // current row
+            Console.Write(Console.CursorTop);
+
+            Console.SetCursorPosition(0,0);
+
+            Console.WriteLine("Hello World");
+
+            Console.ReadKey();
+        }
+    }
+}
