769b9c8525ff72ee917f6b4a495e42e2a6172a94
Mahmood Ramzani <ramzani.mahmood@gmail.com>
2015-06-11 19:38:32 +0430

    mGet

---
+++ Projects/mGet/mGet/Lib/ProtocolType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mGet.Lib
+{
+    public enum ProtocolType
+    {
+        Unknown=0,
+        Http=1,
+    }
+}
